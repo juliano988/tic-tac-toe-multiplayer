8f06c16d051c71222fd6b944d822cc69c3928167
Júlio Faria <julio-pinheiroo@hotmail.com>
2024-02-01 20:27:39 -0300

    Condição de vitoria implementada

--- src/pages/api/game_server.ts
+++ src/pages/api/game_server.ts
@@ -31,13 +31,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
       socket.on("select-emoji", (emoji: string) => {
 
-        socket.emoji = emoji;
-
-        console.log(`User ${socket.id} selected the ${socket.emoji} emoji`);
-
         // The 0 room is the default room, and the 1 room is our game room.
         const roomName = Array.from(socket.rooms)[1];
         const roomPlayers = io.sockets.adapter.rooms.get(roomName);
+
+        socket.emoji = emoji;
+
+        console.log(`[${roomName}] User ${socket.id} selected the ${socket.emoji} emoji`);
 
         // The game starts with 2 players.
         if (roomPlayers?.size === 2) {
@@ -49,12 +49,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
             io.to(roomName).emit("game-start", {
               player1: {
-                id: player1Socket.id,
+                id: player1Socket?.id,
                 emoji: player1Socket.emoji,
                 score: 0
               },
               player2: {
-                id: player2Socket.id,
+                id: player2Socket?.id,
                 emoji: player2Socket.emoji,
                 score: 0
               },
@@ -63,7 +63,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 [0, 0, 0],
                 [0, 0, 0]
               ],
-              turn: player1Socket.id,
+              turn: player1Socket?.id,
               result: null
             });
 
@@ -75,11 +75,78 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
       socket.on('game-update', function (gameObject: Game) {
 
-        console.log(`The game was updated to: ${gameObject.game}`);
+        // The 0 room is the default room, and the 1 room is our game room.
+        const roomName2 = Array.from(socket.rooms)[1];
+
+        console.log(`[${roomName2}] The game was updated to: ${gameObject.game}`);
 
         gameObject.turn = gameObject.turn === gameObject.player1.id ? gameObject.player2.id : gameObject.player1.id;
 
-        console.log(`Now is the turn of player: ${gameObject.turn}`);
+        console.log(`[${roomName2}] Now is the turn of player: ${gameObject.turn}`);
+
+        let primaryDiagonal: Array<0 | 1 | 2> = [];
+        let secondaryDiagonal: Array<0 | 1 | 2> = [];
+
+        // Checking if the game has a winner
+        for (let i = 0; i < gameObject.game.length; i++) {
+
+          const row = gameObject.game[i];
+          const column = [gameObject.game[0][i], gameObject.game[1][i], gameObject.game[2][i]];
+
+          primaryDiagonal.push(gameObject.game[i][i]);
+          secondaryDiagonal.push(gameObject.game[i][gameObject.game.length - 1 - i]);
+
+          if (row.every(function (position) { return position > 0 }) && row.every(function (position, j, arr) { return arr[0] === position })) {
+
+            gameObject.result = row[0];
+
+          } else if (column.every(function (position) { return position > 0 }) && column.every(function (position, j, arr) { return arr[0] === position })) {
+
+            gameObject.result = column[0];
+
+          }
+
+        }
+
+        if (primaryDiagonal.every(function (position) { return position > 0 }) && primaryDiagonal.every(function (position, j, arr) { return arr[0] === position })) {
+
+          gameObject.result = primaryDiagonal[0];
+
+        } else if (secondaryDiagonal.every(function (position) { return position > 0 }) && secondaryDiagonal.every(function (position, j, arr) { return arr[0] === position })) {
+
+          gameObject.result = secondaryDiagonal[0];
+
+        } if (gameObject.game.toString().split(',').indexOf('0') === -1) {
+
+          gameObject.result = 0;
+
+        }
+
+        if (gameObject.result !== null) {
+
+          if (gameObject.result > 0) {
+
+            console.log(`[${roomName2}] Player ${socket.id} is the winner!`);
+
+            if (socket.id === gameObject.player1.id) {
+
+              gameObject.player1.score += 1;
+
+            } else {
+
+              gameObject.player2.score += 1;
+
+            }
+
+          } else {
+
+            console.log(`[${roomName2}] Tied match!`);
+
+          }
+
+        }
+
+        console.log(`[${roomName2}] game: `, gameObject)
 
         // The 0 room is the default room, and the 1 room is our game room.
         const roomName = Array.from(socket.rooms)[1];
